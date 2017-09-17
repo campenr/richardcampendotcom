ebcf9aef36c95501c241616cb5cfc272d8beffa6
Richard Campen <richard@campen.co>
2017-09-17 15:47:04 -0400

    restructure flask app structure to facilitate extending functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,37 @@
+# Copyright 2017 Richard Campen
+# All rights reserved
+# This software is released under the Modified BSD license
+# See LICENSE.txt for the full license documentation
+
 from flask import Flask, render_template
-import requests
-application = Flask(__name__)
+
+app = Flask(__name__)
 
 
-@application.route('/')
-@application.route('/index')
+@app.route('/')
+@app.route('/index')
 def index():
     return render_template("index.html")
 
-@application.route('/publications')
+
+@app.route('/publications')
 def publications():
     return render_template('publications.html')
 
-@application.route('/software')
+
+@app.route('/software')
 def software():
     return render_template('software.html')
 
-@application.route('/projects')
+
+@app.route('/projects')
 def projects():
     return render_template('projects.html')
 
-@application.errorhandler(404)
+
+@app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == "__main__":
-    application.run()
-
-
-# TODO: restructure website to use different pages for about, software, publications, etc.
+    app.run()
---
+++ run.py
@@ -0,0 +1,9 @@
+# Copyright 2017 Richard Campen
+# All rights reserved
+# This software is released under the Modified BSD license
+# See LICENSE.txt for the full license documentation
+
+from app import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
