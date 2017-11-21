1f1feb1d7d76a9d354e023cd4d4b0244d9a2663f
Richard Campen <richard@campen.co>
2017-11-21 08:21:36 -0500

    Default to development enviromment if not specified by the CONFIG_ENV environment variable; simplify celery_app init

--- app/__init__.py
+++ app/__init__.py
@@ -11,33 +11,17 @@ from config import config
 
 flask_app = Flask(__name__)
 
-# load correct config settings based on environment variable
+# load correct config settings based on environment variable, or default to 'development' environment
 config_name = os.environ.get('CONFIG_ENV')
-print('-Loading configuration: ', config_name)
+if config_name is None:
+    print('-Loading configuration: No environment specififed, defaulting to development environment')
+    config_name = 'development'
+else:
+    print('-Loading configuration: ', config_name)
 flask_app.config.from_object(config[config_name])
 
-
-def make_celery(app):
-
-    celery_ = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
-                    broker=app.config['CELERY_BROKER_URL'])
-
-    celery_.conf.update(app.config)
-    TaskBase = celery_.Task
-
-    class ContextTask(TaskBase):
-
-        abstract = True
-
-        def __call__(self, *args, **kwargs):
-
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-
-    celery_.Task = ContextTask
-
-    return celery_
-
-celery_app = make_celery(flask_app)
+# celery setup
+celery_app = Celery(flask_app.name)
+celery_app.conf.update(flask_app.config)
 
 from app import views
