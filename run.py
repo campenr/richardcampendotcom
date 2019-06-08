c68d43b2b6a83c84ba02f4989e8d2b00d958f95d
Richard Campen <richard@campen.co>
2019-06-08 21:10:55 +1200

    fix not being able to access running flask app through vagrant machine;

--- run.py
+++ run.py
@@ -6,4 +6,5 @@
 from app import flask_app
 
 if __name__ == '__main__':
-    flask_app.run(debug=True, port=5000)
+    # flask_app.run(debug=True, host='0.0.0.0', port=5000)
+    flask_app.run(debug=True, host='0.0.0.0')
