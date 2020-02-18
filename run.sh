9ca92523062f81ac3f34995f9357f2659a45c4fd
Richard Campen <richard@campen.co>
2020-02-18 21:15:01 +1300

    feat: switch uwsgi to use vassal mode again, and get run command tailing the uwsgi logs correctly

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 args="$@"
-vagrant ssh -c "sudo uwsgi --ini ./config/uwsgi/richardcampendotcom.uwsgi.ini $args"
+vagrant ssh -c "tail -f -n0 ./logs/uwsgi/richardcampendotcom.log & sudo uwsgi --ini ./config/uwsgi/richardcampendotcom.ini $args"
