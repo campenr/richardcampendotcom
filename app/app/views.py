87f865ce66b75683ab0a9aa05a84be399479a84f
Richard Campen <richard@campen.co>
2020-04-11 15:30:21 +1200

    feat: big code clean up, removing uneeded db table, old software page references, and adding/updating publication page content.

--- app/app/views.py
+++ app/app/views.py
@@ -1,28 +1,20 @@
 from flask import render_template
 from app import flask_app
-from app.models import Software
 
 
 @flask_app.route('/')
-@flask_app.route('/index')
 def index():
     return render_template("index.html")
 
 
 @flask_app.route('/software')
 def software():
-    software_versions = {software_.name: software_.version for software_ in Software.query.all()}
-    return render_template('software.html', software_versions=software_versions)
+    return render_template('software.html')
 
 
 @flask_app.route('/publications')
 def publications():
     return render_template('publications.html')
-
-
-@flask_app.route('/projects')
-def projects():
-    return render_template('projects.html')
 
 
 @flask_app.errorhandler(404)
