8d54626791b8aa2e21f7917c3d9a6387ee8c50e2
Richard Campen <richard@campen.co>
2020-04-11 14:46:28 +1200

    chore: clean up code some, and use js to set copyright date in footer.

--- app/app/trackers.py
+++ app/app/trackers.py
@@ -1,8 +1,3 @@
-# Copyright 2017 Richard Campen
-# All rights reserved
-# This software is released under the Modified BSD license
-# See LICENSE.txt for the full license documentation
-
 from app import db
 from app.models import Software
 import requests
