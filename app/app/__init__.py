e76594184266e18ac7215c33f1ea36c87becd8ef
Richard Campen <richard@campen.co>
2020-04-11 14:26:55 +1200

    feat: upgrade all python packages

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,10 +1,4 @@
-# Copyright 2017 Richard Campen
-# All rights reserved
-# This software is released under the Modified BSD license
-# See LICENSE.txt for the full license documentation
-
 from flask import Flask
-from celery import Celery
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -29,28 +23,8 @@ if environment == 'development':
 # db setup
 db = SQLAlchemy(flask_app)
 
-print('-Configuring celery')
-def make_celery(app):
-    celery_ = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
-                     broker=app.config['CELERY_BROKER_URL'])
-    celery_.conf.update(app.config)
-    TaskBase = celery_.Task
-
-    class ContextTask(TaskBase):
-        abstract = True
-
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-
-    celery_.Task = ContextTask
-
-    return celery_
-
-celery_app = make_celery(flask_app)
-
-print('-Importing views, models, and tasks')
-from app import views, models, tasks
+print('-Importing views, models')
+from app import views, models
 
 print('-Configuring trackers')
 from app import trackers
