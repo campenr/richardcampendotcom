b4906c2bf46b2cfc52200789ec3f1424fd5d2916
Richard Campen <richard@campen.co>
2020-04-13 11:16:45 +1200

    feat: add Flask-Sitemap package to autogenerate a sitemap file at /sitemap.xml

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-vagrant ssh -c "source ./venv/campenco/bin/activate && cd ./campenco/ && python -m pip install -r ./app/requirements.txt"
+vagrant ssh -c "source ./venv/bin/activate && python -m pip install -r ./app/requirements.txt"
