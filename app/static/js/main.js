cb93d725dd53d4bc3eef0f1351e39cee71d0877d
Richard Campen <richard@campen.co>
2020-04-25 15:59:15 +1200

    feat: get basic webpack bundling of js, including bootstrap and it's dependencies.

--- app/static/js/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = main();
-
-function main() {
-    initCopyright();
-}
-
-function initCopyright() {
-    document.getElementById('copyright-year').textContent = String(new Date().getFullYear());
-}
