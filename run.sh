9ea34e19ecab8617b60ad9688d16edf6a1b5f0c8
Richard Campen <richard@campen.co>
2020-08-02 18:13:04 +1200

    feat: add some basic colorisation to run.sh output with grc.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 args="$@"
-vagrant ssh -c "tail -f -n0 ./logs/uwsgi/richardcampendotcom.log & sudo uwsgi --ini ./config/uwsgi/richardcampendotcom.ini $args"
+vagrant ssh -c "grc tail -f -n0 ./logs/uwsgi/richardcampendotcom.log & sudo uwsgi --ini ./config/uwsgi/richardcampendotcom.ini $args"
