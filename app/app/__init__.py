0226cef81bbb652e3fc76dbd70d22e4391a8f2f5
Richard Campen <richard@campen.co>
2021-03-11 21:58:21 +1300

    feat: python cleanup: remove uneeded deps, remove uneeded old db code, update remaining python deps.

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,17 +1,14 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_sitemap import Sitemap
 
 
 flask_app = Flask(__name__)
-flask_app.config.from_object('config')
 
 # enable werkzeug debug traceback when running behind uWSGI
 if flask_app.config.get('DEBUG') is True:
     from werkzeug.debug import DebuggedApplication
     flask_app.wsgi_app = DebuggedApplication(flask_app.wsgi_app, True)
 
-db = SQLAlchemy(flask_app)
 sitemap = Sitemap(flask_app)
 
-from app import views, models, context_processors
+from app import views, context_processors
--- app/config/base.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-from .local import *
